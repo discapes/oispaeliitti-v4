ec358e1e9928da04fe62ccbb241d783392bb5aee
Miika Tuominen <miika.km.tuominen@gmail.com>
2022-11-28 21:42:41 +0200

    fix date logging

--- src/routes/rkv/+server.ts
+++ src/routes/rkv/+server.ts
@@ -1,22 +1,31 @@
 import { redis } from '$lib/model';
 import type { RequestHandler } from './$types';
 
+const TIMEZONE_OFFSET = 2;
+
 function timestamp(date: Date) {
 	return (
-		[date.getFullYear(), date.getMonth() + 1, date.getDate()].join('-') +
+		[
+			date.getFullYear(),
+			date.getUTCMonth().toString().padStart(2, '0') + 1,
+			date.getUTCDate().toString().padStart(2, '0')
+		].join('-') +
 		' ' +
-		[date.getHours(), date.getMinutes()].join(':')
+		[
+			(date.getUTCHours() + TIMEZONE_OFFSET).toString().padStart(2, '0'),
+			date.getUTCMinutes().toString().padStart(2, '0')
+		].join(':')
 	);
 }
 
 export const POST: RequestHandler = async ({ url, request, getClientAddress }) => {
 	const addr = getClientAddress();
-	if (!addr.startsWith('127')) {
+	if (!addr.startsWith('123')) {
 		const [close, db] = await redis();
-		const res = await db.sAdd(
-			'rkv',
-			timestamp(new Date()) + ' - ' + addr + ' - ' + (await request.text())
-		);
+		const res = await db.zAdd('rkv-data', {
+			score: Date.now(),
+			value: timestamp(new Date()) + ' - ' + addr + ' - ' + (await request.text())
+		});
 		close();
 	}
 	return new Response();
