60168b385febed8c9233d949253ae21b6cca3937
Miika Tuominen <miika.km.tuominen@gmail.com>
2022-11-28 15:33:37 +0200

    ...

---
+++ src/routes/rkv/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let name = 'world';
+	let text: string;
+
+	onMount(() => {
+		document.addEventListener('paste', (e) => {
+			text = e.clipboardData!.getData('text');
+			text = [...'123456']
+				.map((n) => {
+					const start = text.indexOf(` :${n}:`) + 4 + 1;
+					const end = text.indexOf(' ', start);
+					return n + `: ` + text.slice(start, end);
+				})
+				.join('\n');
+		});
+	});
+</script>
+
+<div class="bg-dark flex items-center justify-center flex-col p-10 m-10">
+	<h1>Hello {name}!</h1>
+	<pre>{text}</pre>
+</div>
