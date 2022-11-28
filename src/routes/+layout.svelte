9ff4bc1a0cd57804959485707f32928b443d0116
Miika Tuominen <miika.km.tuominen@gmail.com>
2022-11-29 00:12:41 +0200

    vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,19 @@
-<script>
+<script lang="ts">
 	import './tailwind.css';
 	import './normalize.css';
+	import { browser } from '$app/environment';
+	import { webVitals } from '$lib/vitals';
+	import { page } from '$app/stores';
+
+	let analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
+
+	$: if (browser && analyticsId) {
+		webVitals({
+			path: $page.url.pathname,
+			params: $page.params,
+			analyticsId
+		});
+	}
 </script>
 
 <slot />
